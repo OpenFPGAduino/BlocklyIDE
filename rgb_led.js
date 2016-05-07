effcb71b52bbb8672018c17a4513e14f3f11f166
lizhizhou <lzz@meteroi.com>
2016-05-07 17:42:28 +0800

    fpga led work fine.

--- rgb_led.js
+++ rgb_led.js
@@ -10,9 +10,20 @@ Blockly.Blocks['rgb_led'] = {
         .setCheck("Number")
         .appendField("Blue");
     this.setInputsInline(true);
-    this.setOutput(true, "Number");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour(260);
     this.setTooltip('');
     this.setHelpUrl('http://openfpgaduino.github.io/');
   }
+};
+
+Blockly.JavaScript['rgb_led'] = function() {
+  var r = Blockly.JavaScript.valueToCode(this, 'r',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
+  var g = Blockly.JavaScript.valueToCode(this, 'g',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
+  var b = Blockly.JavaScript.valueToCode(this, 'b',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
+  return "fpga.led("+ r +',' + g + ',' + b +")";
 };
