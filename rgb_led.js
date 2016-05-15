0961c2ad5795594658dbfea198c4257aed4d98a4
lizhizhou <lzz@meteroi.com>
2016-05-15 13:30:52 +0800

    Demo test ok

--- rgb_led.js
+++ rgb_led.js
@@ -1,33 +1,40 @@
 Blockly.Blocks['rgb_led'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["F0", "1"], ["F1", "2"], ["F2", "3"], ["F3", "4"]]), "led")
-    this.appendValueInput("r")
-        .setCheck("Number")
-        .appendField("Red");
-    this.appendValueInput("g")
-        .setCheck("Number")
-        .appendField("Green");
-    this.appendValueInput("b")
-        .setCheck("Number")
-        .appendField("Blue");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(260);
-    this.setTooltip('');
-    this.setHelpUrl('http://openfpgaduino.github.io/');
-  }
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown([["F0", "0"], ["F1", "1"], ["F2", "2"], ["F3", "3"]]), "led")
+        this.appendValueInput("r")
+            .setCheck("Number")
+            .appendField("Red");
+        this.appendValueInput("g")
+            .setCheck("Number")
+            .appendField("Green");
+        this.appendValueInput("b")
+            .setCheck("Number")
+            .appendField("Blue");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(260);
+        this.setTooltip('');
+        this.setHelpUrl('http://openfpgaduino.github.io/');
+    }
 };
 
 Blockly.JavaScript['rgb_led'] = function(block) {
-  var led = block.getFieldValue('led');
-  var r = Blockly.JavaScript.valueToCode(this, 'r',
-      Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
-  var g = Blockly.JavaScript.valueToCode(this, 'g',
-      Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
-  var b = Blockly.JavaScript.valueToCode(this, 'b',
-      Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
-  var code = "fpga.led("+ led + ','+ r +',' + g + ',' + b +");\n";
-  return code;
+    var led = block.getFieldValue('led');
+    var r = Blockly.JavaScript.valueToCode(this, 'r',
+        Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
+    var g = Blockly.JavaScript.valueToCode(this, 'g',
+        Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
+    var b = Blockly.JavaScript.valueToCode(this, 'b',
+        Blockly.JavaScript.ORDER_ASSIGNMENT) || '255';
+    //var code = "fpga.led("+ led + ','+ r +',' + g + ',' + b +");\n";
+    var code = 
+    "var url = 'fpga/api/call/led';\n" + 
+    "var xhr = new XMLHttpRequest();\n" +
+    "var post = ["+led+','+ r +','+g+','+b+"];\n" +
+    "xhr.open('post', url, false);\n" + 
+    "xhr.setRequestHeader('Content-Type', 'application/json');\n" +
+    "xhr.send(JSON.stringify(post));"
+    return code;
 };
