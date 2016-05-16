22fc6581a4aa4f42545daf80dc38de32c0b8fd11
lizhizhou <lzz@meteroi.com>
2016-05-16 13:08:36 +0800

    upate color
    upate color

--- wait.js
+++ wait.js
@@ -8,7 +8,7 @@ Blockly.Blocks['wait'] = {
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour(260);
+    this.setColour(120);
     this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
   }
