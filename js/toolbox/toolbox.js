472afec631f208ed731c474bb225083f1e385351
lizhizhou <lzz@meteroi.com>
2016-05-25 13:01:49 +0800

    add toolbox common function
    add toolbox common function

--- js/toolbox/toolbox.js
+++ js/toolbox/toolbox.js
@@ -1,3 +1,19 @@
+function ajax_post(url, json) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('post', url, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    var ret = xhr.send(JSON.stringify(json));
+    return JSON.parser(ret);
+}
+
+function ajax_get(url) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('get', url, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    var ret = xhr.send();
+    return JSON.parser(ret);
+}
+
 var toolbox = ["wait","led", "rgb_led", "temperature","moisture"];
 for( i in toolbox)
 document.write('<script src="js/toolbox/' + toolbox[i] + '.js"></script>\n');
