fe2199372afc6249afa471752a7aa96261d9d7f7
lizhizhou <lzz@meteroi.com>
2016-05-27 14:12:41 +0800

    fix bug

--- js/toolbox/toolbox.js
+++ js/toolbox/toolbox.js
@@ -3,7 +3,10 @@ function ajax_post(url, json) {
     xhr.open('post', url, false);
     xhr.setRequestHeader('Content-Type', 'application/json');
     var ret = xhr.send(JSON.stringify(json));
-    return JSON.parser(ret);
+    if(typeof variable !== "undefined" && variable !== null) {
+    	return JSON.parser(ret);
+    else
+	return ret;
 }
 
 function ajax_get(url) {
