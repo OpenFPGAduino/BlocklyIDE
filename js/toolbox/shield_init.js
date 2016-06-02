3f8dc7f739b0b828b7d8a7c8f52dd83ed4607568
lizhizhou <lzz@meteroi.com>
2016-06-02 12:49:50 +0800

    Add shield init
    Add shield init

---
+++ js/toolbox/shield_init.js
@@ -0,0 +1,17 @@
+Blockly.Blocks['shield_init'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("shield_init");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://openfpgaduino.github.io/');
+  }
+};
+
+Blockly.JavaScript['shield_init'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = "ajax_post('/fpga/api/call/shield_ctrl_init', [])"
+  return code;
+};
