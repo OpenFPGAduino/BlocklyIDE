d82dbd64911efb57328e47c45447ae18c8764053
lizhizhou <lzz@meteroi.com>
2016-07-01 00:19:37 +0800

    fix
    fix

--- js/toolbox/plotpush.js
+++ js/toolbox/plotpush.js
@@ -1,23 +1,28 @@
-Blockly.Blocks['plotpush'] = {
+Blockly.Blocks['plot_push'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Plot");
+        .appendField("PlotP");
     this.appendValueInput("NAME")
         .setCheck("String")
         .appendField("Name");
     this.appendValueInput("Point")
         .setCheck("Number")
         .appendField("Point");
+    this.appendValueInput("colour")
+        .setCheck("Colour")
+        .appendField("Colour");
     this.setInputsInline(false);
     this.setColour(65);
     this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
   }
 };
-Blockly.JavaScript['plotpush'] = function(block) {
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_poiny = Blockly.JavaScript.valueToCode(block, 'Point', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript['plot_push'] = function(block) {
+  var name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  var point = Blockly.JavaScript.valueToCode(block, 'Point', Blockly.JavaScript.ORDER_ATOMIC);
+  var colour = Blockly.JavaScript.valueToCode(block, 'colour', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = 'plotp(' + name + ',' + y +',' + colour +');\n';
   return code;
 };
+   
