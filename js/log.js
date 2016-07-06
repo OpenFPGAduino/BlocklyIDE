ddff3a43f1f0657845bc298b1cb501bf0395e6ac
lizhizhou <lzz@meteroi.com>
2016-07-06 22:27:38 +0800

    Add log.js

---
+++ js/log.js
@@ -0,0 +1,24 @@
+var log = 
+{
+    console:{
+        print :  function (text) {
+        text = text.replace(/\n/gi, '</p><p>');
+        text = text.replace(/\r/gi, '</p><p>');
+        document.getElementById("console").innerHTML += "<p>" + text + "<\p>";
+                },
+        clean :  function () {
+                 document.getElementById("console").innerHTML = " ";
+                }
+    },
+    error:{
+        print :  function (text) {
+        text = text.replace(/\n/gi, '</p><p>');
+        text = text.replace(/\r/gi, '</p><p>');
+        document.getElementById("error").innerHTML += "<p>" + text + "<\p>";
+                },
+        clean :  function () {
+                 document.getElementById("error").innerHTML = " ";
+                }
+    }
+}
+//log.console.print("lizhizhou\n hello world\n");
