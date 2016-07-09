fe529ceef72e5bac172d907e4b7a21e580ef4986
lizhizhou <lzz@meteroi.com>
2016-07-09 14:26:57 +0800

    bug fix
    bug fix

--- js/main.js
+++ js/main.js
@@ -131,52 +131,6 @@ function page_refresh() {
 
 }
 
-function ajax_rest_json_get(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", url, false);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send();
-    debuginf(xhr.responseText);
-    return (JSON.parse(xhr.responseText));
-}
-
-function ajax_rest_json_post(url, json) {
-    var xhr = new XMLHttpRequest();
-    var post;
-    xhr.open("post", url, false);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(json));
-    debuginf(xhr.responseText);
-    return (JSON.parse(xhr.responseText));
-}
-
-function ajax_rest_json_delete(url, json) {
-    var xhr = new XMLHttpRequest();
-    var post;
-    xhr.open("delete", url, false);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(json));
-    debuginf(xhr.status);
-}
-
-
-function ajax_rest_post(url, arg1, arg2) {
-    var xhr = new XMLHttpRequest();
-    var post;
-    xhr.open("post", url, false);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    if (arg1 != undefined)
-        post = encodeURIComponent("arg1") + "=" + encodeURIComponent(arg1) + "&";
-    else
-        post = null;
-    if (arg2 != undefined)
-        post += encodeURIComponent("arg2") + "=" + encodeURIComponent(arg2) + "&";
-    xhr.send(post);
-    debuginf(xhr.status);
-}
-
-
-
 function load_config_list() {
     var xhr = new XMLHttpRequest();
     xhr.open("get", "load_config", false);
