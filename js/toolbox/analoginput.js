38be4c1db3e430f8421df7c182b140a2b9128bb7
lizhizhou <lzz@meteroi.com>
2016-09-27 09:00:24 +0800

    refine code

--- js/toolbox/analoginput.js
+++ js/toolbox/analoginput.js
@@ -21,3 +21,23 @@ Blockly.JavaScript['analoginput'] = function(block) {
     "ajax_post('/fpga/api/call/ain_"+ch+"', ["+ id+"])"
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+Blockly.Blocks['analoginit'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ANALOGINIT);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["A", "a"], ["B", "b"]]), "CH");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://openfpgaduino.github.io/');
+  }
+};
+
+Blockly.JavaScript['analoginit'] = function(block) {
+  var ch = block.getFieldValue('CH');
+  var code = "ajax_post('/fpga/api/call/ain_"+ch+"_init', []);";
+  return code;
+};
--- js/toolbox/digitaloutput.js
+++ js/toolbox/digitaloutput.js
@@ -17,10 +17,39 @@ Blockly.Blocks['digitaloutput'] = {
     this.setHelpUrl('http://openfpgaduino.github.io/');
   }
 };
+
 Blockly.JavaScript['digitaloutput'] = function(block) {
   var io = block.getFieldValue('io');
   var id = Blockly.JavaScript.valueToCode(block, 'ID', Blockly.JavaScript.ORDER_ATOMIC);
   var onoff = block.getFieldValue('onoff');
   var code = "ajax_post('/fpga/api/call/dio_"+io+"_out', ["+ id +","+ onoff +"]);\n";
+  return code;
+};
+
+Blockly.Blocks['digitialdir'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Digital port");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["A", "A"], ["B", "B"]]), "port");
+    this.appendValueInput("ID")
+        .setCheck("Number")
+        .appendField("ID");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["input", "0"], ["output", "1"]]), "dir");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+Blockly.JavaScript['digitialdir'] = function(block) {
+  var dropdown_port = block.getFieldValue('port');
+  var value_id = Blockly.JavaScript.valueToCode(block, 'ID', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_dir = block.getFieldValue('dir');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
   return code;
 };
