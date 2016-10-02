710e116717fe6508c0d0f266866fc5260dd2b42b
lizhizhou <lzz@meteroi.com>
2016-10-02 12:51:52 +0800

    add step motor

--- js/toolbox/stepmotor.js
+++ js/toolbox/stepmotor.js
@@ -1,3 +1,39 @@
+Blockly.Blocks['stepmotorinit'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.STEPMOTOR);
+    this.appendValueInput("ID")
+        .setCheck("Number")
+        .appendField("ID");
+    this.appendValueInput("frequence")
+        .setCheck("Number")
+        .appendField("frequence");
+    this.appendValueInput("duty_cycle")
+        .setCheck("Number")
+        .appendField("duty_cycle");
+    this.appendValueInput("delay")
+        .setCheck("Number")
+        .appendField("delay");    
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.STEPMOTORSTEP);    
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://openfpgaduino.github.io/');
+  }
+};
+
+Blockly.JavaScript['stepmotorinit'] = function(block) {
+  var id = Blockly.JavaScript.valueToCode(block, 'ID', Blockly.JavaScript.ORDER_ATOMIC);
+  var frequence = Blockly.JavaScript.valueToCode(block, 'frequence', Blockly.JavaScript.ORDER_ATOMIC);
+  var duty_cycle = Blockly.JavaScript.valueToCode(block, 'duty_cycle', Blockly.JavaScript.ORDER_ATOMIC);
+  var delay = Blockly.JavaScript.valueToCode(block, 'delay', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "ajax_post('/fpga/api/call/stepmotor_init', ["+id+','+ frequence+','+ duty_cycle +','+ delay+','+"]);\n";
+  return code;
+};
+
 Blockly.Blocks['stepmotor'] = {
   init: function() {
     this.appendDummyInput()
@@ -17,14 +53,13 @@ Blockly.Blocks['stepmotor'] = {
     this.setNextStatement(true, null);
     this.setColour(260);
     this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+    this.setHelpUrl('http://openfpgaduino.github.io/');
   }
 };
 Blockly.JavaScript['stepmotor'] = function(block) {
-  var value_id = Blockly.JavaScript.valueToCode(block, 'ID', Blockly.JavaScript.ORDER_ATOMIC);
+  var id = Blockly.JavaScript.valueToCode(block, 'ID', Blockly.JavaScript.ORDER_ATOMIC);
   var dropdown_step = block.getFieldValue('step');
-  var value_step = Blockly.JavaScript.valueToCode(block, 'step', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var step = Blockly.JavaScript.valueToCode(block, 'step', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "ajax_post('/fpga/api/call/stepmotor', ["+id+','+ dropdown_step+','+ step+"]);\n";
   return code;
 };
