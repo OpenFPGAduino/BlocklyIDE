67f5d230ab7cc47efe2724d7aa9fa8b1f43c49fc
lizhizhou <lzz@meteroi.com>
2016-12-23 22:15:48 +0800

    Add do every

---
+++ js/toolbox/doevery.js
@@ -0,0 +1,24 @@
+Blockly.Blocks['do_every'] = {
+  // Print statement.
+  helpUrl: Blockly.LANG_TEXT_PRINT_HELPURL,
+  init: function() {
+    this.setColour(160);
+    this.appendDummyInput()
+        .appendField('Every')
+        .appendField(new Blockly.FieldTextInput(''), 'MILLIS')
+        .appendField('milli-seconds');
+     this.appendStatementInput('ONTIMEOUT')
+       .appendField('do');
+	this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setTooltip('');
+  }
+};
+
+Blockly.JavaScript.do_every = function() {
+  var branch = Blockly.JavaScript.statementToCode(this, 'ONTIMEOUT');
+  var millis = this.getFieldValue('MILLIS');
+  var id = this.getFieldValue('ID');
+  var code = 'var timer=setInterval(function(){\n' + branch + '\n},' + millis + ');\n\n';
+  return code;
+};
